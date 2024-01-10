5ac6c1e11e0099385145d8724447312576d49edf
Jacob Ceder <jacob.ceder@husqvarnagroup.com>
2024-01-10 15:42:24 +0100

    commit2

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  // Check if the request is for the root URL
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Error loading index.html');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+      }
+    });
+  } else {
+    // Handle other URLs or send a 404 not found response
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('404 Not Found');
+  }
 });
 
 server.listen(PORT, () => {
